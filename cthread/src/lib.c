10d4e9bcf7f51f9c9a96a8d19ff8008730922657
Diego Dimer <diego.dimer@ufrgs.br>
2019-04-13 17:31:47 -0300

    Arrumado link iniciado initqueues

--- cthread/src/lib.c
+++ cthread/src/lib.c
@@ -6,22 +6,59 @@
 #include "../include/cdata.h"
 #include <ucontext.h>
 
+// essa é a thread que está em execução no momento
 TCB_t runningThread;
+
+// thread main
+TBC_t mainThread;
+
+// scheduler: toda thread quando finaliza aponta pra esse contexto. Ele vai ser o responsável
+// por tirar uma thread da fila de aptos e por em execução
+ucontext_t scheduler;
+
+
 int initialized = 0;
 int serialId;
 
+void scheduler(){
+    // troca o processo em execução e coordena as filas
+};
+
+void initQueues(){
+    initialized = 1;
+    // pega o contexto atual pra iniciar o scheduler
+    getcontext(&scheduler);
+    scheduler.uc_link = mainThread; // o scheduler em tese só acaba quando não tem nada nas filas pra escalonar, então volta pra main
+    scheduler.uc_stack.ss_sp = malloc(sizeof(SIGSTKSZ));
+    scheduler.uc_stack.ss_size = SIGSTKSZ;
+
+    makecontext(&(newThread->context), (void(*)(void))&scheduler, 1, arg);
+    /// inicializa as filas
+    // filas apto baixa media e alta prioridade
+    // fila terminado
+    // fila bloqueado
+
+    mainThread = malloc(sizeof(TCB_t));
+    mainThread.tid = 0; // thread main tem o tid = 0
+    mainThread.state = PROCST_EXEC; // main thread ta executando mesmo
+    mainThread.prio = 2; // main tem baixa prioridade
+    getcontext(&(mainThread.context));
+    //descobrir se precisa alocar coisas pra pilha da main thread?
+}
+
 int ccreate (void* (*start)(void*), void *arg, int prio) {
-	initQueues(); // inicializa as filas (todas)
-	
+
+	if(!initialized)
+        initQueues(); // inicializa as filas (todas)
+
 	TCB_t* newThread = malloc(sizeof(TCB_t));
 	newThread->tid = ++serialId;
 	newThread->state = PROCST_APTO;
 	newThread->prio = prio;
 	getcontext(&(newThread->context)); // pega o contexto(atual) e poem no contexto da thread nova
-	// precisa pensar no que poem aqui
-	// newThread->context.uc_link = 
-	// uc_link aponta pro contexto que vai rolar quando essa thread
-	// que eu to definindo aqui acabar
+	// quando cada thread termina ela vai pro scheduler que daí decide qual fila por
+	newThread->context.uc_link = &scheduler;
+
 
 	newThread->context.uc_stack.ss_sp = malloc(sizeof(SIGSTKSZ));
 	newThread->context.uc_stack.ss_size = SIGSTKSZ;
@@ -29,9 +66,9 @@ int ccreate (void* (*start)(void*), void *arg, int prio) {
 
 	//cria um contexto, passas a função start (parametro da ccreate), é 1 parametro pra ela que tá em arg
 	makecontext(&(newThread->context), (void(*)(void))start, 1, arg);
-	
+
 	//append na fila de aptos (ainda nao foi feita)
-	
+
 	return -1;
 }
 
