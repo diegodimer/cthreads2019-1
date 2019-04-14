9fc123e0656eaeb4019c9f96b2802acb241da535
Diego Dimer <diego.dimer@ufrgs.br>
2019-04-13 21:23:41 -0300

    Terminada e testada ccreate

--- cthread/src/lib.c
+++ cthread/src/lib.c
@@ -7,44 +7,51 @@
 #include <ucontext.h>
 
 // essa é a thread que está em execução no momento
-TCB_t runningThread;
+TCB_t *runningThread;
 
 // thread main
-TBC_t mainThread;
+TCB_t *mainThread;
 
 // scheduler: toda thread quando finaliza aponta pra esse contexto. Ele vai ser o responsável
 // por tirar uma thread da fila de aptos e por em execução
-ucontext_t scheduler;
+ucontext_t schedulerC;
 
 
 int initialized = 0;
 int serialId;
 
 void scheduler(){
+	printf("scheduler running");
     // troca o processo em execução e coordena as filas
 };
 
 void initQueues(){
+	
     initialized = 1;
     // pega o contexto atual pra iniciar o scheduler
-    getcontext(&scheduler);
-    scheduler.uc_link = mainThread; // o scheduler em tese só acaba quando não tem nada nas filas pra escalonar, então volta pra main
-    scheduler.uc_stack.ss_sp = malloc(sizeof(SIGSTKSZ));
-    scheduler.uc_stack.ss_size = SIGSTKSZ;
+    getcontext(&schedulerC);
+    schedulerC.uc_link = NULL; // o scheduler em tese só acaba quando não tem nada nas filas pra escalonar, então volta pra main
+    schedulerC.uc_stack.ss_sp = (char *)malloc(sizeof(SIGSTKSZ));
+    schedulerC.uc_stack.ss_size = SIGSTKSZ;
 
-    makecontext(&(newThread->context), (void(*)(void))&scheduler, 1, arg);
+    makecontext(&schedulerC, (void(*)(void))scheduler, 0);
     /// inicializa as filas
     // filas apto baixa media e alta prioridade
     // fila terminado
     // fila bloqueado
 
     mainThread = malloc(sizeof(TCB_t));
-    mainThread.tid = 0; // thread main tem o tid = 0
-    mainThread.state = PROCST_EXEC; // main thread ta executando mesmo
-    mainThread.prio = 2; // main tem baixa prioridade
-    getcontext(&(mainThread.context));
-    //descobrir se precisa alocar coisas pra pilha da main thread?
-}
+    mainThread->tid = 0; // thread main tem o tid = 0
+    mainThread->state = PROCST_APTO; // main thread ta executando mesmo
+    mainThread->prio = 2; // main tem baixa prioridade
+	mainThread->context.uc_link = NULL;
+	mainThread->context.uc_stack.ss_size = SIGSTKSZ;
+	mainThread->context.uc_stack.ss_sp = malloc(sizeof(SIGSTKSZ));
+    getcontext(&(mainThread->context));
+	runningThread = mainThread;
+
+};
+
 
 int ccreate (void* (*start)(void*), void *arg, int prio) {
 
@@ -57,20 +64,18 @@ int ccreate (void* (*start)(void*), void *arg, int prio) {
 	newThread->prio = prio;
 	getcontext(&(newThread->context)); // pega o contexto(atual) e poem no contexto da thread nova
 	// quando cada thread termina ela vai pro scheduler que daí decide qual fila por
-	newThread->context.uc_link = &scheduler;
-
+	newThread->context.uc_link = &schedulerC;
 
 	newThread->context.uc_stack.ss_sp = malloc(sizeof(SIGSTKSZ));
 	newThread->context.uc_stack.ss_size = SIGSTKSZ;
-	// @todo: ver se esse SIGSTKSZ funciona de vdd (im not sure)
-
+	
 	//cria um contexto, passas a função start (parametro da ccreate), é 1 parametro pra ela que tá em arg
 	makecontext(&(newThread->context), (void(*)(void))start, 1, arg);
 
 	//append na fila de aptos (ainda nao foi feita)
-
-	return -1;
+	return newThread->tid;
 }
+
 
 int csetprio(int tid, int prio) {
 	return -1;
